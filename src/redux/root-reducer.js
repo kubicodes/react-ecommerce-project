bad43fc30d3d4c9b6ef9474c747826587bd922cd
kubi <kubilayercikti1@gmail.com>
2021-03-30 21:34:32 +0200

    Using redux persist to store cart data persistantly

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from 'redux'; //to combine all reducers in the root
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //default storage now
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root', //starting point
+  storage,
+  whitelist: ['cart'], //reducers to store
+};
+
+const rootReducer = combineReducers({
   user: userReducer, //key is for the key in the state object
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import { persistStore } from 'redux-persist';
 
 const middlewares = [logger];
-const store = createStore(
+export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
